fb220be59beb580fb007cb587feda8c043410e44
leolbs07 <161033237+leolbs07@users.noreply.github.com>
2024-05-03 12:24:30 +0000

    Aula01

---
+++ 1.Tipos de dados/primeira prog 03/prog03/prog1-03I05.py
@@ -0,0 +1,16 @@
+#tipagem dinamica
+
+nome = "marta"
+idade = 20
+peso = 70.000
+
+#forma nmr 1
+print(nome)
+print(idade)
+print(peso)
+print("\n")
+#forma nmr 2
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Peso: {peso}")
+
---
+++ 1.Tipos de dados/primeira prog 03/prog03/prog2-03I05.py
@@ -0,0 +1,15 @@
+#tipagem estatica
+
+nome: str = "marta"
+idade: int = 20
+peso: float = 70.000
+
+#forma nmr 1
+print(nome)
+print(idade)
+print(peso)
+print("\n")
+#forma nmr 2
+print(f"Nome : {nome}")
+print(f"Idade : {idade}")
+print(f"Peso : {peso}")
